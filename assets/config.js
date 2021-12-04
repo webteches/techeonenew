f3022caf425abb3d0a3803fe81a601bab19cbf59
TechOne <39878816+webteches@users.noreply.github.com>
2021-12-05 00:02:17 +0100

    Add files via upload

---
+++ assets/config.js
@@ -0,0 +1,24 @@
+const config = {
+    
+    USERNAME: "[TB] Dev.techeone-techone#0568",
+    PICTURE: "assets/img/profile_picture.png",
+    
+        DESCRIPTION: "Hi 👋, I'm techeone ! I am French and <b>Précident de L'asso RedProjet</b> and I'm Developer Web/Node.js/Skript",
+    
+        BADGES: [
+            { id: "HypeSquad Brilliance ", image: "https://static.miraheze.org/discordwiki/c/c7/48cf0556d93901c8cb16317be2436523.svg" }
+            ],
+    
+        PROJECTS: [
+            { id: "project-1", image: "https://cdn.discordapp.com/icons/602554814253105162/62d4d140db321bc2a726646100c3bc8a.webp", link: "https://discord.gg/R3Wr3JhbcB", name: "TechOne-Bot" },
+            { id: "project-2", image: "https://cdn.discordapp.com/attachments/904417356250488862/907662035066699787/LogoCByAddylox500x500.png", link: "https://discord.cubexland.fr", name: "CubexLand" },
+            { id: "project-3", image: "https://media.discordapp.net/attachments/639417116692578307/650474115232301066/80c9bffe-4482-476a-88dd-0b85c899d173-preview_1.png", link: "https://discord.gg/xjHJHGPyE3", name: "RedProjet" },
+        ],
+    
+        SOCIALS: [
+            { class_name: "fab fa-github", link: "https://github.com/webteches" },
+            { class_name: "fab fa-twitter", link: "https://twitter.com/techeone2" },
+            { class_name: "fab fa-discord", link: "https://discord.cubexland.fr" },
+        ],
+    
+    }
